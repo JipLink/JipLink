9bc6c16fc37e0bb8589ea1bff235ad491d859fac
Hyeonm1339 <jhjimess@naver.com>
2025-10-01 16:17:11 +0900

    Default Spring Backend Setup

---
+++ backend/src/main/java/com/focuson/jiplink/JiplinkApplication.java
@@ -0,0 +1,13 @@
+package com.focuson.jiplink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiplinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiplinkApplication.class, args);
+	}
+
+}
